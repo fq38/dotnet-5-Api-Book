7419d9f3552cc79a90c2353a8cb3babc398fa1b9
fq38 <fq38.live.com>
2022-08-28 11:27:45 -0300

    Adicionar arquivos de projeto.

---
+++ WebApi5shirleyrios/Repositories/IBookRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApi5shirleyrios.Model;
+
+namespace WebApi5shirleyrios.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get(); 
+        
+        Task<Book> Get(int id);
+
+        Task<Book> Create(Book book);
+
+        Task update(Book book);
+       
+        Task Delete(int id);
+
+
+
+    }
+}
